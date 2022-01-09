9d71a3fadbe1d1c426262ecbb1be0ccc46bfcc28
mossabdiae <mossabdiae@gmail.com>
2022-01-09 15:03:50 +0100

    added snippet_detail view

--- snippets/views.py
+++ snippets/views.py
@@ -23,3 +23,32 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    # 1- check if it actually exist
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    # 2- implement your accepted http verbs 
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    # Notice how the PUT/POST (writting) methods always includes an is_valid test
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
