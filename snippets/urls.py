1534d766bc33d45cec9d879ac9adc9bdb365a32f
mossabdiae <mossabdiae@gmail.com>
2022-01-10 01:28:43 +0100

    adding support for different formats

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+# taking the original urlpatterns and generating a new version with taking care of format
+# adding something like this : <URLPattern 'snippets<drf_format_suffix:format>'>
+urlpatterns = format_suffix_patterns(urlpatterns)
